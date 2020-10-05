c8c0aa50efbaed47f8101ef58b077d287524e702
Matthew Lock <matthew_lock20@hotmail.com>
2020-10-05 13:37:44 +0100

    Simple click counter added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+// import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Container } from 'reactstrap'
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
 <Container style= {{ marginTop: 20}}>
-  <Button color='success' >Start</Button>
+  <p className= 'text-primary'>You clicked {count} times</p>
+  <Button onClick={() => setCount(count+1)} color='success'>Increase</Button>
+  <Button onClick={()=> setCount(count-1)} color='danger'>Decrease</Button>
 </Container>
+
   );
 }
 

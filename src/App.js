d9602eb9807eecae4104b76409c576d8a5b73e3b
Matthew Lock <matthew_lock20@hotmail.com>
2020-10-05 18:11:05 +0100

    Default state of fixed costs showing with header

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Container } from 'reactstrap'
 import React, { useState } from 'react';
+import Tracker from './Tracker'
+import { Jumbotron } from 'reactstrap'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-<Container style= {{ marginTop: 20}}>
-  <p className= 'text-primary'>You clicked {count} times</p>
-  <Button onClick={() => setCount(count+1)} color='success'>Increase</Button>
-  <Button onClick={()=> setCount(count-1)} color='danger'>Decrease</Button>
-</Container>
-
-  );
+    < Tracker />
+  )
 }
 
 export default App;
---
+++ src/Tracker.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { Jumbotron } from 'reactstrap'
+import logo from './logo.svg'
+import { Button, Container } from 'reactstrap'
+
+const EXPENSE = [
+  {id: 1, name: "buy a book", amount: 10},
+  {id: 2, name: "buy cd", amount: 5}
+]
+
+function Tracker() {
+const[expenses, setExpenses] = useState(EXPENSE)
+
+return (
+  <Container className='text-center'>
+    <Jumbotron fluid>
+      <h2 className= 'display-6'>
+        Expenses Tracker React App
+      <br></br>
+        <img src={logo} style={{ width: 50, height: 50 }} alt='react-logo' />
+      </h2>
+      <div>
+        <p>
+          Total Expense: {''}
+          <span className='text success'>
+            £{''}
+            {expenses.reduce((accumulator, currentValue) => {
+              return (accumulator += parseInt(currentValue.amount))
+            }, 0)}
+          </span>
+        </p>
+      </div>
+    </Jumbotron>
+  </Container>
+)
+}
+
+export default Tracker
